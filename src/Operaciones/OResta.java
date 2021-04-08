344b648e29dd8cd6dbd19384c45685c41cd71ea7
hugo4295 <hugo4295@victor-HP>
2021-04-07 20:01:24 -0500

    una descripcion del proyecto

---
+++ src/Operaciones/OResta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operaciones;
+
+/**
+ *
+ * @author hugo4295
+ */
+public class OResta {
+    public int restar(int x, int y){
+        return (x-y);
+    }
+    
+}
